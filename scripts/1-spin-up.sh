92e925c0e9f05a2093a40107999067f0f98e4299
wdurno <wdurno@kpmg.com>
2020-08-03 02:13:20 +0000

    cleaned-up spin-up

--- scripts/1-spin-up.sh
+++ scripts/1-spin-up.sh
@@ -6,8 +6,3 @@ cd ../scripts
 gcloud container clusters get-credentials ha-demo-cluster --zone us-central1-a --project gdax-dnn
 ## install api on k8s 
 helm install api ../helm/api/
-## initialize database 
-POSTGRES_POD=$(kubectl get pods | tail -n+2 | grep postgres | awk '{print $1;}') 
-kubectl exec -it $POSTGRES_POD -- psql -U postgres -c "CREATE DATABASE api" 
-kubectl exec -it $POSTGRES_POD -- psql -U postgres -d api -c "CREATE TABLE visits(total INT4)" 
-kubectl exec -it $POSTGRES_POD -- psql -U postgres -d api -c "INSERT INTO visits(total) VALUES (0)" 
