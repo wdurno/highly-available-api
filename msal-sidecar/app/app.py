fd5139d1611a82fa7412f217d3badb54fdb1e12c
wdurno <wdurno@gmail.com>
2020-10-19 00:41:32 +0000

    drafting msal auth soln

---
+++ msal-sidecar/app/app.py
@@ -0,0 +1,116 @@
+import uuid
+import requests
+from flask import Flask, render_template, session, request, redirect, url_for, jsonify
+from flask_session import Session  # https://pythonhosted.org/Flask-Session
+import msal
+import argparse 
+
+app = Flask(__name__)
+Session(app)
+
+parser = argparse.ArgumentParser(description='Azure Active Directory authentication sidecar app') 
+parser.add_argument('--client-id', required=True, type=str, help='AAD client ID')
+parser.add_argument('--client-secret', required=True, type=str, help='AAD client secret') 
+parser.add_argument('--authority', required=True, type=str, help='AAD Oauth2 authority, example: https://login.microsoftonline.com/Enter_the_Tenant_Name_Here') 
+parser.add_arugment('--callback-path', type=str, default='/oauth2/callback', help='recieve tokens here') 
+parser.add_argument('--downstream', required=True, type=str, help='MSAL-sidecar forwards request to this URL after authentication') 
+
+# This section is needed for url_for("foo", _external=True) to automatically
+# generate http scheme when this sample is running on localhost,
+# and to generate https scheme when it is deployed behind reversed proxy.
+# See also https://flask.palletsprojects.com/en/1.0.x/deploying/wsgi-standalone/#proxy-setups
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+@app.route("/")
+def index():
+    if not session.get("user"):
+        return redirect(url_for("login"))
+    return _downstream()  
+
+@app.route("/login")
+def login():
+    session["state"] = str(uuid.uuid4())
+    # Technically we could use empty list [] as scopes to do just sign in,
+    # here we choose to also collect end user consent upfront
+    auth_url = _build_auth_url(scopes=[], state=session["state"])
+    return render_template("login.html", auth_url=auth_url, version=msal.__version__)
+
+@app.route(app_config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
+def authorized():
+    if request.args.get('state') != session.get("state"):
+        return redirect(url_for("index"))  # No-OP. Goes back to Index page
+    if "error" in request.args:  # Authentication/Authorization failure
+        return render_template("auth_error.html", result=request.args)
+    if request.args.get('code'):
+        cache = _load_cache()
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+            request.args['code'],
+            scopes=app_config.SCOPE,  # Misspelled scope would cause an HTTP 400 error here
+            redirect_uri=url_for("authorized", _external=True))
+        if "error" in result:
+            return render_template("auth_error.html", result=result)
+        session["user"] = result.get("id_token_claims")
+        _save_cache(cache)
+    return redirect(url_for("index"))
+
+@app.route("/logout")
+def logout():
+    session.clear()  # Wipe out user and its token cache from session
+    return redirect(  # Also logout from your tenant's web session
+        app_config.AUTHORITY + "/oauth2/v2.0/logout" +
+        "?post_logout_redirect_uri=" + url_for("index", _external=True))
+
+def _downstream():
+    ## get valid token 
+    token = _get_token_from_cache(app_config.SCOPE)
+    if not token:
+        return redirect(url_for("login"))
+    ## call downstream 
+    data = requests.get(  # Use token to call downstream service
+        app_config.ENDPOINT,
+        headers={'Authorization': 'Bearer ' + token['access_token']},
+        ).json()
+    return jsonify(data) 
+
+def _load_cache():
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
+    return cache
+
+def _save_cache(cache):
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
+
+def _build_msal_app(cache=None, authority=None):
+    return msal.ConfidentialClientApplication(
+        app_config.CLIENT_ID, authority=authority or app_config.AUTHORITY,
+        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+
+def _build_auth_url(authority=None, scopes=None, state=None):
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for("authorized", _external=True))
+
+def _get_token_from_cache(scope=None):
+    cache = _load_cache()  # This web app maintains one cache per session
+    cca = _build_msal_app(cache=cache)
+    accounts = cca.get_accounts()
+    if accounts:  # So all account(s) belong to the current signed-in user
+        result = cca.acquire_token_silent(scope, account=accounts[0])
+        _save_cache(cache)
+        return result
+
+if __name__ == "__main__":
+    args = argparser.parse_args() 
+    app.config.update(
+            CLIENT_ID=args.client_id,
+            CLIENT_SECRET=args.client_secret,
+            AUTHORITY=args.authority,
+            REDIRECT_PATH=args.callback_path,
+            ENDPOINT=args.downstream 
+            )
+    app.run()
+
